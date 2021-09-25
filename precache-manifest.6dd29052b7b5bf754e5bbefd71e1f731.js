818981b052cf23d30e93f8012a23e1dd048fd54b
hamidhosseini <h.hosseini@estechgroup.io>
2021-09-25 10:17:52 +0100

    Initial commit

---
+++ precache-manifest.6dd29052b7b5bf754e5bbefd71e1f731.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c132c9d70ce3fe98aa41",
+    "url": "/css/app.5e04de37.css"
+  },
+  {
+    "revision": "5c417847191fb7486fe7",
+    "url": "/css/chunk-vendors.fa50644f.css"
+  },
+  {
+    "revision": "e631d2735799aa943d93d301abf423d2",
+    "url": "/fonts/Lato-Black.e631d273.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/fonts/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "eddd6ba79b35a7933ceaffd559a93714",
+    "url": "/img/axa.eddd6ba7.png"
+  },
+  {
+    "revision": "bbc85f4f081e83e88b601e8c7f51c05d",
+    "url": "/img/defaqto.bbc85f4f.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "7e7413af173325034e17916bb9060415",
+    "url": "/img/logo.7e7413af.png"
+  },
+  {
+    "revision": "f761a0a954726ef11da0e0d5fed0851b",
+    "url": "/img/lv.f761a0a9.png"
+  },
+  {
+    "revision": "fc3251a8e4889006fbb0e05aabc6302e",
+    "url": "/img/prestige.fc3251a8.png"
+  },
+  {
+    "revision": "d133c99a798991a201e14125d4aaa8bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c132c9d70ce3fe98aa41",
+    "url": "/js/app.6bc9dcee.js"
+  },
+  {
+    "revision": "5c417847191fb7486fe7",
+    "url": "/js/chunk-vendors.c13eb634.js"
+  },
+  {
+    "revision": "4e2e437f5a24cf560911be0978b10043",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
